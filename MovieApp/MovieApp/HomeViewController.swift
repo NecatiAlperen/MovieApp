22b9cb7d059144bd5eadce92ef74f8ca902e05dd
Necati Alperen IŞIK <109997859+NecatiAlperen@users.noreply.github.com>
2024-02-21 17:51:34 +0300

    Add files via upload

---
+++ MovieApp/MovieApp/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  MovieApp
+//
+//  Created by Necati Alperen IŞIK on 21.02.2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+ 
+
+}
